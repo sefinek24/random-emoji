ae07e4db6ae6d23c33714367bf01fd60e4feaee4
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-15 07:49:56 +0200

    Updated test file

--- test.js
+++ test.js
@@ -1,31 +1,31 @@
-const { unicode, emojis, cats, hearts, foods, circles, kaomojis } = require('./index.js');
+const random = require('./index.js');
 
 // A single emote
-console.log(unicode());
+console.log(random.unicode());
 
 // Random emoji with name and type
-const emoji = emojis();
+const emoji = random.emojis();
 console.log(`Name: ${emoji.name}; Emoji: ${emoji.content};`);
 
 // Random cats
-const cat = cats();
+const cat = random.cats();
 console.log(`Name: ${cat.name}; Emoji: ${cat.content};`);
 
 // Random hearts
-const heart = hearts();
+const heart = random.hearts();
 console.log(`Name: ${heart.name}; Emoji: ${heart.content};`);
 
 // Random foods
-const food = foods();
+const food = random.foods();
 console.log(`Name: ${food.name}; Emoji: ${food.content};`);
 
 // Random circles
-const circle = circles();
+const circle = random.circles();
 console.log(`Name: ${circle.name}; Emoji: ${circle.content};`);
 
 // Random kaomoji
 (async () => {
-	const kaomoji = new kaomojis();
+	const kaomoji = new random.kaomojis();
 	const kaoCat = await kaomoji.cat();
 	console.log(`Cat face: ${kaoCat.message}`);
 
