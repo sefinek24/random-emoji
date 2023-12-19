5e87f1c1cb1d61ee358fa2be41f070863e5765e7
Sefinek <contact@sefinek.net>
2023-12-19 23:15:34 +0100

    Quality fixes

--- examples/table.js
+++ examples/table.js
@@ -3,40 +3,40 @@ const data = [];
 
 async function displayEmojiData() {
 	// Fetches a single random Unicode emoji.
-	data.push({ Function: 'unicode()', Name: 'N/A', Emoji: random.unicode() });
+	data.push({ Function: 'unicode()', Emoji: random.unicode().trim() });
 
 	// Retrieves a random emoji with its name, visual representation, and type.
 	const emoji = random.emojis();
-	data.push({ Function: 'emojis()', Name: emoji.name, Emoji: emoji.content, Type: emoji.type });
+	data.push({ Function: 'emojis()', Emoji: emoji.content.trim(), Name: emoji.name, Type: emoji.type });
 
 	// Gets a random cat emoji with its name and visual representation.
 	const cat = random.cats();
-	data.push({ Function: 'cats()', Name: cat.name, Emoji: cat.content });
+	data.push({ Function: 'cats()', Emoji: cat.content.trim(), Name: cat.name });
 
 	// Fetches a random heart emoji with its name and visual representation.
 	const heart = random.hearts();
-	data.push({ Function: 'hearts()', Name: heart.name, Emoji: heart.content });
+	data.push({ Function: 'hearts()', Emoji: heart.content.trim(), Name: heart.name });
 
 	// Retrieves a random food emoji with its name and visual representation.
 	const food = random.foods();
-	data.push({ Function: 'foods()', Name: food.name, Emoji: food.content });
+	data.push({ Function: 'foods()', Emoji: food.content.trim(), Name: food.name });
 
 	// Gets a random circle emoji with its name and visual representation.
 	const circle = random.circles();
-	data.push({ Function: 'circles()', Name: circle.name, Emoji: circle.content });
+	data.push({ Function: 'circles()', Emoji: circle.content.trim(), Name: circle.name });
 
 	// Gets a random squares emoji with its name and visual representation.
-	const squares = random.circles();
-	data.push({ Function: 'squares()', Name: squares.name, Emoji: squares.content });
+	const squares = random.squares();
+	data.push({ Function: 'squares()', Emoji: squares.content.trim(), Name: squares.name });
 
 
 	// Fetches random kaomojis
 	const kaomojis = new random.Kaomojis();
 	const kaoCat = await kaomojis.cat();
-	data.push({ Function: 'cat()', Name: 'Cat face', Emoji: kaoCat.message });
+	data.push({ Function: 'cat()', Emoji: kaoCat.message.trim(), Name: 'Cat face' });
 
 	const uwu = await kaomojis.uwu();
-	data.push({ Function: 'uwu()', Name: 'UwU', Emoji: uwu.message });
+	data.push({ Function: 'uwu()', Emoji: uwu.message.trim(), Name: 'UwU' });
 
 
 	// Displays the table
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const { get } = require('https');
-const { version } = require('./package.json');
-
 const emojis = {
 	emojis: require('./data/emoji/collection/big.json'),
-
 	cats: require('./data/emoji/cat.json'),
 	foods: require('./data/emoji/food.json'),
 	hearts: require('./data/emoji/heart.json'),
-
 	circles: require('./data/emoji/circle.json'),
 	squares: require('./data/emoji/square.json'),
-
 	unicode: require('./data/emoji/collection/single.json'),
 };
-
 const endpoints = require('./data/endpoints.json');
+const { version } = require('./package.json');
 
-/**
- * Options for HTTP GET requests.
- * @type {Object}
- */
 const httpOptions = {
 	method: 'GET',
 	port: 443,
@@ -37,41 +28,31 @@ const httpOptions = {
 	},
 };
 
-/**
- * Fetches content from a URL.
- * @param {string} url - URL to fetch content from.
- * @returns {Promise<Object>} - Promise resolving to the fetched JSON data.
- */
+
 async function getContent(url) {
-	try {
-		const res = await new Promise((resolve, reject) => {
-			const req = get(url, httpOptions, resolve);
-			req.on('error', reject);
-			req.end();
-		});
+	const res = await new Promise((resolve, reject) => {
+		const req = get(url, httpOptions, resolve);
+		req.on('error', reject);
+		req.end();
+	});
 
-		if (res.statusCode !== 200) {
-			throw new Error(`Request failed with status code ${res.statusCode}`);
-		}
-
-		res.setEncoding('utf8');
-		let rawData = '';
-		for await (const chunk of res) {
-			rawData += chunk;
-		}
-		return JSON.parse(rawData);
-	} catch (err) {
-		throw err;
+	if (res.statusCode !== 200) {
+		throw new Error(`Request failed with status code ${res.statusCode}`);
 	}
+
+	res.setEncoding('utf8');
+	let rawData = '';
+	for await (const chunk of res) {
+		rawData += chunk;
+	}
+
+	return JSON.parse(rawData);
 }
 
-/**
- * SefinekAPI class for accessing various random content endpoints.
- */
 class SefinekAPI {
 	constructor() {
-		Object.keys(endpoints).forEach(endpoint => {
-			this[endpoint] = () => getContent(`https://api.sefinek.net/api/v2/random/${endpoints[endpoint]}`);
+		Object.entries(endpoints).forEach(([key, endpoint]) => {
+			this[key] = () => getContent(`https://api.sefinek.net/api/v2/random/${endpoint}`);
 		});
 	}
 }
@@ -80,7 +61,6 @@ module.exports = {
 	...Object.fromEntries(Object.entries(emojis).map(([key, value]) => [
 		key, () => value[Math.floor(Math.random() * value.length)],
 	])),
-
 	Kaomojis: SefinekAPI,
 	version,
 };
