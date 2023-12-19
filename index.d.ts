313e606bf5c6693dac65045eeaee133b955ae303
Sefinek <contact@sefinek.net>
2023-12-19 22:20:01 +0100

    Add `.npmignore`

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,70 @@
 declare module '@sefinek/random-emoji' {
-    // Unicode
-    export function unicode(): string;
-
-
-    // Random emojis
-    export interface Emoji {
+    /**
+     * Represents an emoji with name, content, and type.
+     * Used for the collection of emojis with more detailed information.
+     */
+    export interface EmojisCollection {
         content: string;
         name: string;
         type: string;
     }
-    export function emojis(): Emoji;
-    export function cats(): Emoji;
-    export function hearts(): Emoji;
-    export function foods(): Emoji;
-    export function circles(): Emoji;
 
+    /**
+     * Represents an emoji with name and content.
+     * Used for simpler emoji categories like cats, hearts, etc.
+     */
+    export interface Emojis {
+        content: string;
+        name: string;
+    }
 
-    // Kaomojis
+    /**
+     * Returns a random emoji from a big collection.
+     * Each emoji includes its name and type.
+     * @returns {EmojisCollection} A random emoji from the collection.
+     */
+    export function emojis(): EmojisCollection;
+
+    /**
+     * Returns a random cat emoji.
+     * @returns {Emojis} A random cat emoji.
+     */
+    export function cats(): Emojis;
+
+    /**
+     * Returns a random circle emoji.
+     * @returns {Emojis} A random circle emoji.
+     */
+    export function circles(): Emojis;
+
+    /**
+     * Returns a random food emoji.
+     * @returns {Emojis} A random food emoji.
+     */
+    export function foods(): Emojis;
+
+    /**
+     * Returns a random heart emoji.
+     * @returns {Emojis} A random heart emoji.
+     */
+    export function hearts(): Emojis;
+
+    /**
+     * Returns a random square emoji.
+     * @returns {Emojis} A random square emoji.
+     */
+    export function squares(): Emojis;
+
+    /**
+     * Returns a random Unicode emoji character.
+     * @returns {string} A random Unicode emoji.
+     */
+    export function unicode(): string;
+
+    /**
+     * Represents the response from an API endpoint.
+     * Includes information about the success status, API category, and the response message.
+     */
     export interface ApiResponse {
         success: boolean;
         status: number;
@@ -26,14 +74,24 @@ declare module '@sefinek/random-emoji' {
         };
         message: string;
     }
+
+    /**
+     * Kaomojis class for accessing various random content endpoints.
+     * Allows fetching of random kaomojis from specified endpoints.
+     */
     export class Kaomojis {
-        cat(): Promise<ApiResponse>;
-        owo(): Promise<ApiResponse>;
-        uwu(): Promise<ApiResponse>;
-        love(): Promise<ApiResponse>;
+        constructor();
+
+        /**
+         * Dynamically generated methods for accessing random content endpoints.
+         * Each method returns a Promise that resolves to an ApiResponse.
+         * @returns {Promise<ApiResponse>} A promise resolving to the API response.
+         */
+        [endpoint: string]: () => Promise<ApiResponse>;
     }
 
-
-    // Other
+    /**
+     * The current version of the module.
+     */
     export const version: string;
 }
